c7d0cfbc99c8906e1f063dfbb7fa09b7034c9e47
omarman573 <omarnorddin0@gmail.com>
2026-02-15 23:24:02 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+function search(){
+    let page = document.getElementById("page").value;
+    document.getElementById("result").innerHTML =
+    "sayfa" + page + " icin cozum burada gorunecek 😎"
+}
