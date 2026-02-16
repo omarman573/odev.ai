3f5cf3f517938b89eca143049e0b0e92c784444e
omarman573 <omarnorddin0@gmail.com>
2026-02-16 19:56:11 +0300

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-function search(){
-    let page = document.getElementById("page").value;
-    document.getElementById("result").innerHTML =
-    "sayfa" + page + " icin cozum burada gorunecek 😎"
-}
